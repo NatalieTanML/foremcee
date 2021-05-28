2ace9efadf4e49a16e3c488d4f9c38bb7cab7e58
Chua Bing Quan <chipneverdies@gmail.com>
2021-05-29 05:55:22 +0800

    Implement audio recording interface

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Recorder from '@/views/Recorder'
+
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'menu',
       component: require('@/components/Menu').default
+    },
+    {
+      path: '/recorder',
+      name: 'recorder',
+      component: Recorder
     },
     {
       path: '*',
