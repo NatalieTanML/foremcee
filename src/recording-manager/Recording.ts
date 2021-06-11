de39c3ee1f5d71b0e93f515f2ec73d36d053cc44
Chua Bing Quan <chipneverdies@gmail.com>
2021-06-11 20:21:54 +0800

    Implement webm to wav conversion

--- src/recording-manager/Recording.ts
+++ src/recording-manager/Recording.ts
@@ -1,6 +1,8 @@
 import { promisify } from 'util';
 import { access } from 'fs';
 import path from 'path';
+import { spawn } from 'child_process';
+import ffmpegPath from 'ffmpeg-static';
 
 const accessAsync = promisify(access);
 
@@ -11,6 +13,33 @@ const exists = async (dir: string): Promise<boolean> => {
   } catch (err) {
     return false;
   }
+};
+
+const webmToWav = async (
+  webmFileDir: string,
+  destDir: string,
+  wavFileName: string
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const ffmpeg = spawn(ffmpegPath, [
+      // eslint-disable-next-line prettier/prettier
+      '-i', webmFileDir,
+      // eslint-disable-next-line prettier/prettier
+      '-ar', '16000',
+      // eslint-disable-next-line prettier/prettier
+      '-y', path.join(destDir, `${wavFileName}.wav`),
+    ]);
+    let output = '';
+    ffmpeg.stderr.on('data', (chunk) => {
+      output += chunk;
+    });
+    ffmpeg.on('exit', (code) => {
+      if (code) {
+        return reject(new Error(output));
+      }
+      return resolve();
+    });
+  });
 };
 
 export default class Recording {
@@ -36,18 +65,21 @@ export default class Recording {
     const wavExist = await exists(wavPath);
 
     if (!wavExist) {
-      const webmPath = path.join(
+      const webmFilePath = path.join(
         this.#directory,
         `${Recording.defaultAudioName}.webm`
       );
 
-      // TODO: Convert webm audio to wav using ffmpeg and store it.
+      await webmToWav(
+        webmFilePath,
+        this.#directory,
+        Recording.defaultAudioName
+      );
     }
 
     return wavPath;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async getTranscript(): Promise<string> {
     const txtPath = path.join(this.#directory, this.#defaultTranscriptFilename);
     const txtExist = await exists(txtPath);
